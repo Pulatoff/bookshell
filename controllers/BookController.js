b943903e5e90d6a1809da558b78af8db44d05490
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-24 13:55:28 +0500

    error handling writed

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,40 +1,57 @@
 const Book = require("../models/BookModel");
 const axios = require("axios");
+const AppError = require("../utils/AppError");
 
 exports.addBook = async (req, res, next) => {
-  const { isbn } = req.params;
-  const response = await axios.get(`https://openlibrary.org/isbn/${isbn}.json`);
+  try {
+    const { isbn } = req.params;
 
-  const { authors, number_of_pages, title, publish_date } = response.data;
-  const key = authors[0].key;
-  const responsetAuthor = await axios.get(`https://openlibrary.org${key}`);
-  const author = responsetAuthor.data;
-  const book = await Book.create({
-    title,
-    number_of_pages,
-    author: author.name,
-    isbn,
-    first_publishing_year: publish_date,
-  });
+    const response = await axios.get(
+      `https://openlibrary.org/isbn/${isbn}.json`
+    );
 
-  res.status(201).json({
-    status: "success",
-    data: book,
-  });
+    const { authors, number_of_pages, title, publish_date } = response.data;
+    const key = authors[0].key;
+    const responsetAuthor = await axios.get(`https://openlibrary.org${key}`);
+    const author = responsetAuthor.data;
+
+    const book = await Book.create({
+      title,
+      number_of_pages,
+      author: author.name,
+      isbn,
+      first_publishing_year: publish_date,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: book,
+    });
+  } catch (e) {
+    next(new AppError(e.message, 404));
+  }
 };
 
 exports.readBook = async (req, res, next) => {
-  const { isbn, status } = req.body;
-  const book = await Book.findOne({ isbn });
-  if (!book) throw new Error("Book not found");
-  book.status = +status;
-  book.save();
-  res.status(203).json({ status: "success" });
+  try {
+    const { isbn, status } = req.body;
+    const book = await Book.findOne({ isbn });
+    if (!book) throw new Error("Book not found");
+    book.status = +status;
+    book.save();
+    res.status(203).json({ status: "success" });
+  } catch (error) {
+    next(new AppError(error.message, 404));
+  }
 };
 
 exports.getBooks = async (req, res, next) => {
-  const books = await Book.find();
-  res
-    .status(200)
-    .json({ status: "success", result: books.length, data: books });
+  try {
+    const books = await Book.find();
+    res
+      .status(200)
+      .json({ status: "success", result: books.length, data: books });
+  } catch (error) {
+    next(new AppError(error.message, 404));
+  }
 };
--- middlewares/app.js
+++ middlewares/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const errorController = require("../controllers/errorController");
+const AppError = require("../utils/AppError");
 
 app.use(express.json());
 
@@ -11,4 +13,9 @@ app.use(morgan("dev"));
 
 app.use("/", require("../routes/bookRoute"));
 
+app.all("*", (req, res, next) => {
+  return next(new AppError("This page has not defined", 404));
+});
+
+app.use(errorController);
 module.exports = app;
